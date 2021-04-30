44ba5edf05e67095ee1a6434847878962b002f57
daniel <danilugones10@gmail.com>
2021-04-30 14:22:55 +0200

    30-04-2021

---
+++ js/mujer.js
@@ -0,0 +1,34 @@
+const caja=document.getElementById("cajilla");
+
+fetch('http://localhost:8080/producto/2')
+    // .then(res => response.ok ? Promise.resolve(res) : Promise.reject(res))
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const fragment=document.createDocumentFragment()
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].categoria.id_categoria)
+            const div=document.createElement('div')
+            const cabezera = document.createElement('h5')
+            const cuerpo =document.createElement('p')
+            const img=document.createElement('img')
+            const precio=document.createElement('p')
+            const boton =document.createElement('button')
+            cabezera.textContent = `${data[i].nombre_producto}`
+            img.src= `${data[i].imagen}`
+            precio.textContent=`${data[i].precio}€`
+            cuerpo.textContent=`${data[i].marca} - ${data[i].color}`
+            boton.id=`boton ${data[i].id_producto}`;boton.className="btn btn-primary"
+            boton.textContent="Añadir"
+            div.className="sudaderasMujer"
+            div.id=`ficha ${data[i].id_producto}`
+            div.appendChild(cabezera)
+            div.appendChild(cuerpo)
+            div.appendChild(img)
+            div.appendChild(precio)
+            div.appendChild(boton)
+            fragment.appendChild(div)
+        }
+        caja.appendChild(fragment)
+    })
+    .catch(err=> console.log(`error: ${err.status}`))
